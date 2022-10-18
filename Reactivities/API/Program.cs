d99b5b7a63db78911fcc68da403a31c249232ab7
Stuart Murray <stuart.murray.dev@outlook.com>
2022-10-18 12:13:28 +0100

    checkpoint

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,10 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 namespace API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -1,24 +1,29 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Persistence;
 
 namespace API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+
+            services.AddDbContext<DataContext>(options => {
+                options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
         }
 
