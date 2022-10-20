8df1d3d3bb85f3c154e5f25872c739ab85bdc3a6
Stuart Murray <stuart.murray.dev@outlook.com>
2022-10-20 13:48:46 +0100

    cleanup startup file

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using AutoMapper;
 using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -21,27 +22,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-
-            services.AddMediatR(typeof(List.Handler).Assembly);
-
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
