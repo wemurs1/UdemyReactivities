ee633c6a0497e02d05c60fc08ef58b77307d5dae
Stuart Murray <stuart.murray.dev@outlook.com>
2022-11-02 09:41:47 +0000

    client error handling

--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -18,10 +18,16 @@ axios.interceptors.response.use(
     return response;
   },
   (error: AxiosError<any>) => {
-    const { data, status } = error.response!;
+    const { data, status, config } = error.response!;
 
     switch (status) {
       case 400:
+        if (typeof data === 'string') {
+          toast.error(data);
+        }
+        if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+          history.push('/not-found');
+        }
         if (data.errors) {
           const modelStateErrors = [];
           for (const key in data.errors) {
@@ -30,8 +36,6 @@ axios.interceptors.response.use(
             }
           }
           throw modelStateErrors.flat();
-        } else {
-          toast.error(data);
         }
         break;
       case 401:
