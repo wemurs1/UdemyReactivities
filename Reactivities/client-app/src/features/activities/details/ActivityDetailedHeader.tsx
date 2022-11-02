9079075042856564e4c8846851c49ece76aa5c38
Stuart Murray <stuart.murray.dev@outlook.com>
2022-11-02 17:39:55 +0000

    use forms. format dates

--- Reactivities/client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ Reactivities/client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -39,7 +40,7 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
                   content={activity.title}
                   style={{ color: 'white' }}
                 />
-                <p>{activity.date}</p>
+                <p>{format(activity.date!, 'dd MMM yyyy')}</p>
                 <p>
                   Hosted by <strong>Bob</strong>
                 </p>
--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,18 @@
 import { observer } from 'mobx-react-lite';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
+import MySelectInput from '../../../app/common/form/MySelectInput';
+import { categoryOptions } from '../../../app/common/options/categoryOptions';
+import MyDateInput from '../../../app/common/form/MyDateInput';
+import { Activity } from '../../../app/models/activity';
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -19,18 +25,23 @@ export default observer(function ActivityForm() {
     loadingInitial,
   } = activityStore;
   const { id } = useParams<{ id: string }>();
-  const [activity, setActivity] = useState({
+  const [activity, setActivity] = useState<Activity>({
     id: '',
     title: '',
     category: '',
     description: '',
-    date: '',
+    date: null,
     city: '',
     venue: '',
   });
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity title is required'),
+    description: Yup.string().required('The activity description is required'),
+    category: Yup.string().required(),
+    date: Yup.string().required(),
+    venue: Yup.string().required(),
+    city: Yup.string().required(),
   });
 
   useEffect(() => {
@@ -80,18 +91,22 @@ export default observer(function ActivityForm() {
       >
         {({ values: activity, handleChange, handleSubmit }) => (
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-            <FormField>
-              <Field placeholder='Title' name='title' />
-              <ErrorMessage
-                name='title'
-                render={(error) => <Label basic color='red' content={error} />}
-              />
-            </FormField>
-            <Field placeholder='Description' name='description' />
-            <Field placeholder='Category' name='category' />
-            <Field placeholder='Date' name='date' type='date' />
-            <Field placeholder='City' name='city' />
-            <Field placeholder='Venue' name='venue' />
+            <MyTextInput name='title' placeholder='Title' />
+            <MyTextArea rows={3} placeholder='Description' name='description' />
+            <MySelectInput
+              options={categoryOptions}
+              placeholder='Category'
+              name='category'
+            />
+            <MyDateInput
+              placeholderText='Date'
+              name='date'
+              showTimeSelect
+              timeCaption='time'
+              dateFormat='MMMM d, yyyy h:mm aa'
+            />
+            <MyTextInput placeholder='City' name='city' />
+            <MyTextInput placeholder='Venue' name='venue' />
             <Button
               loading={loading}
               floated='right'
