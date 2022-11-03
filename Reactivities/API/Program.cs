6af283ff213642293b633462630dadc77128969d
Stuart Murray <stuart.murray.dev@outlook.com>
2022-11-03 10:41:53 +0000

    add identity and account controller (basic)

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -16,8 +18,9 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -24,6 +24,7 @@ namespace API
             });
 
             services.AddApplicationServices(_configuration);
+            services.AddIdentityServices(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
