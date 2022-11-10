c88b10ee9fa740390669961f215443b5c0b25c1f
Stuart Murray <stuart.murray.dev@outlook.com>
2022-11-10 18:54:45 +0000

    checkpoint

--- Reactivities/API/Extensions/ApplicationServiceExtensions.cs
+++ Reactivities/API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,7 @@
 using Application.Activities;
 using Application.Core;
 using Application.Interfaces;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -38,6 +39,10 @@ namespace API.Extensions
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
             services.AddScoped<IUserAccessor, UserAccessor>();
+
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+
+            services.Configure<CloudinarySettings>(configuration.GetSection("Cloudinary"));
 
             return services;
         }
