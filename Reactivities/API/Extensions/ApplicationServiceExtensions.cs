32b03f8e2d9607f51d3e7f9fb61b1ec45ae5f8f3
Stuart Murray <stuart.murray.dev@outlook.com>
2022-11-15 12:55:55 +0000

    checkpoint

--- Reactivities/API/Extensions/ApplicationServiceExtensions.cs
+++ Reactivities/API/Extensions/ApplicationServiceExtensions.cs
@@ -30,7 +30,10 @@ namespace API.Extensions
             {
                 options.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithOrigins("http://localhost:3000");
                 });
             });
 
